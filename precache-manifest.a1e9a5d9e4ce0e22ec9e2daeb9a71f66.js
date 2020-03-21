970ad6dd928f9706461e41b0481a675abff6da8d
levaniCS <levanisarishvili3105@gmail.com>
2020-03-21 15:44:47 +0400

    Updates

---
+++ precache-manifest.a1e9a5d9e4ce0e22ec9e2daeb9a71f66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bf9ca70226473d4af6f3ce0f11b136d",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "bedd2adbfdab3cb681d8",
+    "url": "/tic-tac-toe/static/css/main.ddfd5a96.chunk.css"
+  },
+  {
+    "revision": "4fde2cd788482483d7ae",
+    "url": "/tic-tac-toe/static/js/2.39df647a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.39df647a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bedd2adbfdab3cb681d8",
+    "url": "/tic-tac-toe/static/js/main.6377f9ca.chunk.js"
+  },
+  {
+    "revision": "b83c3f5dfddd51f16016",
+    "url": "/tic-tac-toe/static/js/runtime-main.e9088102.js"
+  }
+]);
